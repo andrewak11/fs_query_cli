bc975f7a1f0bc2016fb4f325611cfea60f0acf6e
Andrew Akhash <36655762+andrewak11@users.noreply.github.com>
2022-11-23 00:50:51 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
+import os
+import gzip
+import mimetypes
+import shutil
 
 from os import listdir
-from os import isfile, join
+from os.path import isfile, join
 
 def filesInDirectory(mydir: str):
     onlyfiles = [ f for f in listdir(mydir) if isfile(join(mydir, f)) ]
     return onlyfiles
 
 print(filesInDirectory('test_dir'))
+print(mimetypes.guess_type('test_dir/sam3.gz'))
+
+cur_path = os.path.dirname(__file__)
+
+with open(os.path.join(os.getcwd(), 'test_dir/sam3.txt'), 'rb') as f:
+    with gzip.open('test_dir/sam3.gz', 'wb') as zf:
+        shutil.copyfileobj(f, zf)
